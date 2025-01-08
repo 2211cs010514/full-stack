45583e8e5cc0f67160d412e4bca449773d8ad46a
2211cs010514 <2211cs010514@mallareddyuniversity.ac.in>
2025-01-08 13:18:00 +0530

    new file added

---
+++ looping.js
@@ -0,0 +1,46 @@
+// for(let i=1; i<=5;i++){
+//      console.log("Java script")
+//}
+
+
+for (let i = 1; i <=100; i++) {
+      console.log(i);
+    
+}
+
+
+// factorial of 5
+function factorial(number) {
+    let result = 1;
+
+    for (let i = 1; i <= number; i++) {
+        result *= i; // Multiply result by the current value of i
+    }
+
+    return result;
+}
+
+const number = 5;
+console.log(`The factorial of ${number} is: ${factorial(number)}`);
+
+
+//! to find the factors of a number 
+// 12-2,4,3,6
+
+// ! while loop
+let i = 1;
+while (i<=5)
+{
+    console.log("get up");
+    i++;
+    
+}
+
+
+// do while
+let j=1;
+do 
+{
+    j++;
+    console.log("Hello Gajni")
+} while (j>=5);
